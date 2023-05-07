355aae7553491441b1ad8c9878c32907e7981ce2
Takuto <Takuto.72.soccer@gmail.com>
2023-05-07 12:32:45 +0900

    Initial Commit

---
+++ memodef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memodef
+//
+//  Created by 小野拓人 on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
